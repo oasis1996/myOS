dc852ef4899393afa066968cc1afbe87cdc89603
Red Pig <dangnoykung@hotmail.com>
2011-09-03 11:21:07 +0700

    Initial Commit

---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Apple on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
